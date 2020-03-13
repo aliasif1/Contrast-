0d894a37bfae8269414c23220bb18693ddc49676
aliasif1 <56556743+aliasif1@users.noreply.github.com>
2020-03-13 12:26:47 -0600

    Update installation_Kvazaar_ubuntu.sh

--- installation_Kvazaar_ubuntu.sh
+++ installation_Kvazaar_ubuntu.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+git clone https://github.com/ultravideo/kvazaar.git
+cd kvazaar
 sudo apt-get update -y
 sudo apt-get install automake -y
 sudo apt-get install autoconf -y
 sudo apt-get install libtool -y
 sudo apt-get install m4 -y
+sudo apt-get install build-essential -y
 sudo apt-get install yasm -y
+
+./autogen.sh
+./configure
+make
+sudo make install
+
 sudo ldconfig
 echo "Successfully installed Kvazaar - run Kvazaar --version for verifying the installation"
